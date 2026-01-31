985f4a42dbcc1492f3811a4ee05a3d095e86607b
Pc-28 <Pc-28@Pc28>
2026-01-31 10:06:17 -0600

    codigopractica

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pc-28
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el primer número:");
+        int a = sc.nextInt();
+
+        System.out.println("Ingrese el segundo número:");
+        int b = sc.nextInt();
+
+      //  Suma suma = new Suma();
+       // Resta resta = new Resta();
+       // Multiplicacion multiplicacion = new Multiplicacion();
+
+       // System.out.println("Suma: " + suma.calcular(a, b));
+       // System.out.println("Resta: " + resta.calcular(a, b));
+       // System.out.println("Multiplicación: " + multiplicacion.calcular(a, b));
+
+        
+    }
+    
+}
